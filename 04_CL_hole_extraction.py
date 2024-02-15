5cc8ea63fab84e698a561586fc1089bbc3a611c5
Fischer Robert <robert.fischer@psi.ch>
2024-02-15 13:20:18 +0100

    odd step size

--- 04_CL_hole_extraction.py
+++ 04_CL_hole_extraction.py
@@ -40,7 +40,7 @@ def find_free_GPU_memory(gpu_id, limit=0.5):
 
 def hole_scan_3D(im3D, radius=2, iterations = 100, njobs=5):
     mask = im3D<1
-    scan_pos = range(0, mask.shape[1]-1, 2)
+    scan_pos = range(0, mask.shape[1]-1, 3)
     results = Parallel(n_jobs=njobs, temp_folder=temppath)(delayed(diffusion_3D)(mask, scan_pos[i], radius, iterations, i%5) for i in range(len(scan_pos)))
     diffs_stack = np.stack(results, axis=0)
     diff_mean = diffs_stack.mean(axis=0)
@@ -89,7 +89,7 @@ def image_function(file):
     
     im = skimage.io.imread(path)
     im = im.transpose(0,2,1)[:,40:,:]
-    im = im>0
+    im = im==0
     
     diff_mean = hole_scan_3D(im)
     holes1 = diff_mean[:,0,:]>-0.5
