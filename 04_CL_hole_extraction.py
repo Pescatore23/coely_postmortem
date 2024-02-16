b428c165baaa21d4e4ec101b3a89f7ef07eaec15
Fischer Robert <fische_r@mpc2959.psi.ch>
2024-02-16 16:02:55 +0100

    reverted erosion

--- 04_CL_hole_extraction.py
+++ 04_CL_hole_extraction.py
@@ -56,7 +56,7 @@ def diffusion_3D(mask, free_pos, radius, iterations = 100, gpu_id=0):
     with cp.cuda.Device(gpu_id):
         structure = GPUball(radius)
         mask = cp.array(mask)
-        mask = GPUndimage.binary_erosion(mask, GPUball(1))
+        #mask = GPUndimage.binary_erosion(mask, GPUball(1))
         init = cp.zeros(mask.shape, dtype=bool)
         init[:,free_pos,:] = True
 
