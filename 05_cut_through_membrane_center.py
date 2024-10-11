0abbafaeba9d19a1b5bb1f3d9ae1ce67e819391f
Fischer Robert <robert.fischer@psi.ch>
2024-10-11 14:23:42 +0200

    cut through membrane

--- 05_cut_through_membrane_center.py
+++ 05_cut_through_membrane_center.py
@@ -135,7 +135,7 @@ def series_function(series, n_jobs = 8):
     
     
 series = ['A', 'B', 'C', 'Z']
-series = ['D', 'E']
+series = ['D', 'E', 'F', 'G']
 Parallel(n_jobs = 4, temp_folder=temppath)(delayed(series_function)(ser) for ser in series)
         
         
