1af82d979a1ab78c48dde80c8559a7355a482bf8
Fischer Robert <robert.fischer@psi.ch>
2024-10-17 13:39:15 +0200

    extract volumetric histograms

--- 07_CL_projection.py
+++ 07_CL_projection.py
@@ -14,6 +14,7 @@ from cucim.skimage.morphology import ball as GPUball
 from time import sleep 
 import os
 from joblib import Parallel, delayed
+import xarray as xr
 
 temppath = '/mnt/SSD/fische_r/tmp'
 toppath = '/mnt/nas_nanotomData/CT_Data_PSI/FR54/2023_COELY_postmortem'
@@ -21,6 +22,8 @@ outpath = os.path.join(toppath, 'ACL_projections_mean')
 if not os.path.exists(outpath):
     os.mkdir(outpath)
 
+
+bins=np.arange(0,1.4,0.025)
 
 def find_free_GPU_memory(gpu_id, limit=0.75, num_GPU = 5):
     free = cp.cuda.Device(gpu_id).mem_info[0]/cp.cuda.Device(gpu_id).mem_info[1]
@@ -72,13 +75,17 @@ def load_segmented(ser, sample, stage):
     impath = os.path.join(toppath, sertop+'_CL_segmented', ser+'_'+sample+'_'+stage+'__ACL_segmented.tif')
     im = np.transpose(skimage.io.imread(impath), (1,2,0))<1
     return im
+
+def CL_hist(im, bins = bins):
+    return np.histogram(im, bins=bins)[0]
     
 def project_CL(im, imseg, gpu_id):
     im_trace = pad_and_close3D(imseg, gpu_id)
     im = im.astype(float)
     im[~im_trace] = np.nan
     proj = np.nanmean(im, axis=1)
-    return proj
+    hist = CL_hist(im)
+    return proj, hist
 
 def extract_samples(series):
     samples = []
@@ -105,18 +112,29 @@ def sample_function(sample_name, i):
     imseg = load_segmented(ser, sample, stage)
     
     gpu_id = find_free_GPU_memory(i%5)
-    proj = project_CL(im, imseg, gpu_id)
+    proj, hist = project_CL(im, imseg, gpu_id)
     
-    path = os.path.join(outpath, ser+'_'+sample+'_'+stage+'_ACL_projection.tif')
-    skimage.io.imsave(path, proj)
+    # path = os.path.join(outpath, ser+'_'+sample+'_'+stage+'_ACL_projection.tif')
+    # skimage.io.imsave(path, proj)
+    return hist
 
 
-series = ['A','B','C','D','E','F']
+series = ['A','B','C','D','E','F', 'G']
 samples = extract_samples(series)
 
-Parallel(n_jobs = 32, temp_folder=temppath)(delayed(sample_function)(samples[i], i) for i in range(len(samples)))
+results = Parallel(n_jobs = 32, temp_folder=temppath)(delayed(sample_function)(samples[i], i) for i in range(len(samples)))
 
 #create sample list
+results = np.stack(results)
+
+data = xr.dataset({'volume_hist': (['sample', 'bin'], results)},
+                  coords = {'sample': samples,
+                            'bin': bins}
+    )
+
+data.to_netcdf(os.path.join(toppath, 'CL_grayvalue_histograms.nc'))
+
+
 
  
     
